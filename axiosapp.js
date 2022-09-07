e0acd171639f21e0eb123b31a31ada0fe06b8025
Collins Kariuki <collinsmunkary@gmail.com>
2022-09-07 15:32:21 +0300

    refactor code to allow inputting a variable in the request

--- axiosapp.js
+++ axiosapp.js
@@ -12,9 +12,14 @@
 
 
 //REFACTOR THE ABOVE CODE AS AN ASYNCHRONOUS FUNCTION
-const getStarWarsPerson = async () => {
-    const res = await axios.get("https://swapi.dev/api/people/1");
-    console.log(res.data);
+const getStarWarsPerson = async (id) => {
+    try{
+        const res = await axios.get(`https://swapi.dev/api/people/${id}/`);
+        console.log(res.data);
+    }
+    catch (e){
+        console.log("ERROR: ", e)
+    }
 }
 
-getStarWarsPerson();
+getStarWarsPerson(15);
