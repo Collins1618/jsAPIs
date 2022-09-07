9e331cf02fb08efc86ac199cb39ce903da9052e4
Collins Kariuki <collinsmunkary@gmail.com>
2022-09-07 15:26:04 +0300

    Refactor the code to use axios in an asynchrounous function

--- axiosapp.js
+++ axiosapp.js
@@ -1,10 +1,20 @@
 //makes a request and parses the json for us
-axios.get("https://swapi.dev/api/people/1")
-.then(res => {
-    console.log("RESPONSE: ", res);
-})
+// axios.get("https://swapi.dev/api/people/1")
+// .then(res => {
+//     console.log("RESPONSE: ", res);
+// })
 
-//to catch the error that may result from our code
-.catch((e) => {
-    console.log("ERROR! ", e)
-})
+// //to catch the error that may result from our code
+// .catch((e) => {
+//     console.log("ERROR! ", e)
+// })
+
+
+
+//REFACTOR THE ABOVE CODE AS AN ASYNCHRONOUS FUNCTION
+const getStarWarsPerson = async () => {
+    const res = await axios.get("https://swapi.dev/api/people/1");
+    console.log(res.data);
+}
+
+getStarWarsPerson();
