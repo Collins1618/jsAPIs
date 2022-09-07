fa32515cae5ad9eb70857bab90e55a2eff260378
Collins Kariuki <collinsmunkary@gmail.com>
2022-09-07 14:36:45 +0300

    add another request

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ fetch("https://swapi.dev/api/people/1")
 })
 .then(data => {
     console.log(data);
+    return fetch("https://swapi.dev/api/people/5")
+})
+.then(res => {
+    console.log("REQUEST 2");
+    return res.json(); //get the complete response
+    //.json returns a promise so we have to use a .then again
+})
+.then(data => {
+    console.log(data)
 })
 .catch((e) => {
     console.log("ERROR!, e");
